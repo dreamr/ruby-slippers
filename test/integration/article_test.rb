3305a29db4cb5b8590f5db46432ca44416e95357
James OKelly <james@rubyloves.me>
2011-06-28 23:04:42 -0700

    updated integration tests

--- test/integration/article_test.rb
+++ test/integration/article_test.rb
@@ -14,7 +14,7 @@ module RubySlippers::Engine
     end
 
     context "GET a single article" do
-      setup { @ruby_slippers.get("/2010/05/17/the-wonderful-wizard-of-oz") }
+      setup { @ruby_slippers.get("/2010/02/17/the-wonderful-wizard-of-oz") }
       asserts("returns a 200")                { topic.status }.equals 200
       asserts("content type is set properly") { topic.content_type }.equals "text/html"
       should("contain the article")           { topic.body }.includes_html("p" => /Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum./)
